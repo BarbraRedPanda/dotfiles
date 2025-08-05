96ca2a3df8a47014680de0c9b036c134f19967ce
BarbraRedPanda <parsak9000@gmail.com>
2025-08-05 17:57:02 -0400

    Autoconnect to no-pass networks
    Added conditional statement so user is not prompted for password when
    network doesn't require a password

--- .config/rofi/wifi/wifi.sh
+++ .config/rofi/wifi/wifi.sh
@@ -1,8 +1,20 @@
 dir="$HOME/.config/rofi"
 
-bssid=$(nmcli -f SSID,BARS device wifi list | awk 'NR==1 {print; next} {if ($1 == "--") $1="Hidden Network"; print}' | sed '1d' | rofi -theme $HOME/.config/rofi/themes/wifi.rasi -dmenu -p " " -lines 10 | awk '{print $1}')
+# Get BSSID of selected network with rofi
+bssid=$(nmcli -f SSID,BARS,BSSID device wifi list | awk 'NR==1 {print; next} {if ($1 == "--") $1="Hidden Network"; print}' | sed '1d' | rofi -theme $HOME/.config/rofi/themes/wifi.rasi -dmenu -p " " -lines 10 | awk '{print $NF}')
+
+# Exit if no selection
 [ -z "$bssid" ] && exit 1
-pass=$(echo "" | rofi -theme $HOME/.config/rofi/themes/wifi.rasi -dmenu -theme-str 'textbox-prompt-colon {str: "";}' -p "Enter password")
-[ -z "$pass" ] && notify-send "🔑 Password not entered" && exit 1
-nmcli device wifi connect $bssid password $pass
+
+# Save security of given WiFi
+security=$(nmcli -f SECURITY,BSSID device wifi list | grep "$bssid" | awk '{print $1}')
+
+# Prompt for pass if required
+if [ "$security" != '--'] && [ -n "$security" ]
+  pass=$(echo "" | rofi -theme $HOME/.config/rofi/themes/wifi.rasi -dmenu -theme-str 'textbox-prompt-colon {str: "";}' -p "Enter password")
+  [ -z "$pass" ] && notify-send "🔑 Password not entered" && exit 1
+  nmcli device wifi connect $bssid password $pass
+else
+  nmcli device wifi connect $bssid
+
 notify-send " New WiFi Connected"
