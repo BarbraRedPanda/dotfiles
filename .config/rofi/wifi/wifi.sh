8aad9386c0531b57631385431dbc4d03259a320d
BarbraRedPanda <parsak9000@gmail.com>
2025-08-05 16:37:51 -0400

    Beautified while truncating first line

--- .config/rofi/wifi/wifi.sh
+++ .config/rofi/wifi/wifi.sh
@@ -1,6 +1,6 @@
 dir="$HOME/dotfiles/.config/rofi"
 
-bssid=$(nmcli -f SSID,BARS device wifi list | sed -n '1!P' | rofi -theme $HOME/.config/rofi/themes/wifi.rasi -dmenu -p " " -lines 10 | awk '{print $1}')
+bssid=$(nmcli -f SSID,BARS device wifi list | awk 'NR==1 {print; next} {if ($1 == "--") $1="Hidden Network"; print}' | sed '1d' | rofi -theme $HOME/.config/rofi/themes/wifi.rasi -dmenu -p " " -lines 10 | awk '{print $1}')
 [ -z "$bssid" ] && exit 1
 pass=$(echo "" | rofi -theme $HOME/.config/rofi/themes/wifi.rasi -dmenu -theme-str 'textbox-prompt-colon {str: "";}' -p "Enter password")
 [ -z "$pass" ] && notify-send "🔑 Password not entered" && exit 1
