d329c7cc3d3d1595c5ac56f0f6e306c3eb9b68ad
BarbraRedPanda <parsak9000@gmail.com>
2025-08-05 16:30:13 -0400

    Beautified wifi output

--- .config/rofi/wifi/wifi.sh
+++ .config/rofi/wifi/wifi.sh
@@ -1,8 +1,8 @@
 dir="$HOME/dotfiles/.config/rofi"
 
-bssid=$(nmcli device wifi list | sed -n '1!P' | cut -b 9- | rofi -theme $HOME/.config/rofi/themes/topright.rasi -dmenu -p " " -lines 10 | awk '{print $1}')
+bssid=$(nmcli -f SSID,BARS device wifi list | sed -n '1!P' | rofi -theme $HOME/.config/rofi/themes/wifi.rasi -dmenu -p " " -lines 10 | awk '{print $1}')
 [ -z "$bssid" ] && exit 1
-pass=$(echo "" | rofi -theme $HOME/parsa/.config/rofi/themes/topright.rasi -dmenu -theme-str 'textbox-prompt-colon {str: "";}' -p "Enter password")
+pass=$(echo "" | rofi -theme $HOME/.config/rofi/themes/wifi.rasi -dmenu -theme-str 'textbox-prompt-colon {str: "";}' -p "Enter password")
 [ -z "$pass" ] && notify-send "🔑 Password not entered" && exit 1
 nmcli device wifi connect $bssid password $pass
 notify-send "📶 New WiFi Connected"
