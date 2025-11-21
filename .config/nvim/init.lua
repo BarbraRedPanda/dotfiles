710cf2b392e82be859bf03f9f6500f260527c10b
BarbraRedPanda <parsak9000@gmail.com>
2025-11-20 19:21:39 -0500

    Fixed wifi

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,3 +5,11 @@ vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = true, silent = true })
 
 -- Exit visual mode by typing 'jk'
 vim.api.nvim_set_keymap("v", "jk", "<Esc>", { noremap = true, silent = true })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "markdown", "tex" },
+  callback = function()
+    vim.opt_local.wrap = true -- soft wrap
+    vim.opt_local.linebreak = true -- wrap on word boundaries, not mid-word
+  end,
+})
